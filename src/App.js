752b23dc73a1c07a3205bc538fcb118ac5fae897
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-11 22:56:38 -0400

    creating a Layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Layout>
+          <p>Text</p>
+        </Layout>
+      </div>
+    );
   }
 }
 
