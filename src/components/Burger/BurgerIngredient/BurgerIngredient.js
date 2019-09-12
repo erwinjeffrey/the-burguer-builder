5eefed8a30e78635020db2bab2d6501da05e682b
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-11 23:50:06 -0400

    adding a dynamic ingredient Component

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './BurgerIngredient.css'
+const burgerIngredient = (props) => {
+    let ingredient =  null;
+
+    switch(props.type){
+        case ('bread-bottom'):
+            ingredient = <div className={classes.Bread}>
+        
+    }
+}
