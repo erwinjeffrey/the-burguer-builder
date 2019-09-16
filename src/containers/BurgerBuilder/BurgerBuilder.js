445c14ac6f070e0501da9c921e8855b47a4ef528
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-16 15:36:30 -0400

    Implementing the Backdrop Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
   purchaseHandler = () => {
     this.setState({ purchasing: true });
   };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -84,7 +88,10 @@ class BurgerBuilder extends Component {
     // {salad:true, meat: false, ...}
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
