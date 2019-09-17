0bdd74056119064b929e12150dd1d6f334add7b7
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-16 22:35:05 -0400

    Creating a Responsive Sidedrawer

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+
+const sideDrawer = props => {
+  // will only show up for mobile divices
+  return (
+    <div className={classes.SideDrawer}>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
+};
+
+export default sideDrawer;
