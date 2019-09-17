b2c3974202a9599daa04eb3248ccb499a8180245
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-17 16:47:28 -0400

    the burger builder -basic version: we have seen the core of angular so far the build a complete app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 
 class App extends Component {
+  // So far so good, this a complete react course, using the latest fundation
+  // until this step you are able to build a full react app, although we're going to enhance it
+  // it more. but this is the core to build a full react app.
   render() {
     return (
       <div>
