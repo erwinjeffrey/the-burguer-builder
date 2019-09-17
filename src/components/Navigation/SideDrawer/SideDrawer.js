037a4cea6f2801d1a58c3b5f68c9023786ffe5f0
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-16 22:48:31 -0400

    Working on Responsive adjustsments part 1

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,7 +8,7 @@ const sideDrawer = props => {
   // will only show up for mobile divices
   return (
     <div className={classes.SideDrawer}>
-      <Logo />
+      <Logo height="11%" />
       <nav>
         <NavigationItems />
       </nav>
