81f78c730df895cc0423d0399c44a5639ce7353c
System Administrator <root@Maxs-MacBook-Pro.local>
2019-09-26 11:08:03 -0400

    Navigating Back & To Next Page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,10 +10,21 @@ class Checkout extends Component {
       bacon: 1
     }
   };
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('checkout/contact-data');
+  };
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
     );
   }
