4e7c2e5abb5537607758e9148a1fb2ecdaba926e
System Administrator <root@Maxs-MacBook-Pro.local>
2019-10-02 22:58:42 -0400

    Finishing the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,14 +31,14 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
-    axios
+    /* axios
       .get('https://react-my-burger-81b65.firebaseio.com/ingredients.json')
       .then(response => {
         this.setState({ ingredients: response.data });
       })
       .catch(error => {
         this.setState({ error: true });
-      });
+      });*/
   }
   updatePurchaseState(ingredients) {
     const sum = Object.keys(ingredients)
