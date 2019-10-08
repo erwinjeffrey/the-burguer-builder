52cdc44ff354813529692f8d3185b8f2da38caa6
System Administrator <root@Maxs-MacBook-Pro.local>
2019-10-08 14:36:06 -0400

    Redux Advanced Chap: Preparing the folder structure

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
+import reducer from './store/reducers/order';
 
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
