37275921cd18115d49424fd0b758bdc0bc760585
System Administrator <root@Maxs-MacBook-Pro.local>
2019-10-14 15:35:51 -0400

    fecthing Orders via Redux: fixing bug

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -42,3 +42,45 @@ export const purchaseInit = () => {
     type: actionTypes.PURCHASE_INIT
   };
 };
+
+export const fetchOrdersSuccess = orders => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = error => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    axios
+      .get('https://react-my-burger-81b65.firebaseio.com/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        //converting this javascript object to an array
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch(err => {
+        dispatch(fetchOrdersFail(err));
+      });
+  };
+};
--- src/store/reducers/order.1.js
+++ src/store/reducers/order.1.js
@@ -1,7 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-  orders: [],
+  order: [],
   loading: false,
   purchased: false
 };
