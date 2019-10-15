34055ba385c25f1c12d993b257ed1f94f7033eee
System Administrator <root@Maxs-MacBook-Pro.local>
2019-10-15 16:55:56 -0400

    Logging Users out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -27,6 +27,10 @@ const authFail = (state, action) => {
     loading: false
   });
 };
+
+const authLogout = (state, action) => {
+  return updateObject(state, { token: null, userId: null });
+};
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
@@ -35,6 +39,8 @@ const reducer = (state = initialState, action) => {
       return authSuccess(state, action);
     case actionTypes.AUTH_FAIL:
       return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT:
+      return authLogout(state, action);
     default:
       return state;
   }
