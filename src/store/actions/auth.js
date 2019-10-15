55eadc5715df1366fa22eca2e94db7090407ee4b
System Administrator <root@Maxs-MacBook-Pro.local>
2019-10-14 23:46:03 -0400

    Getting a Token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +24,23 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCtmXbgAqar6j0fVLOwfnOt1EA6EKqde8U',
+        authData
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail());
+      });
   };
 };
