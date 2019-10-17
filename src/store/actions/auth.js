df1cbf48921271ce82a759151414f9be05700ce7
System Administrator <root@Maxs-MacBook-Pro.local>
2019-10-16 22:47:35 -0400

    Fixing Connect + Routing Errors

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -79,19 +79,23 @@ export const setAuthRedirectPath = path => {
 };
 
 export const authCheckState = () => {
-    return  dispatch => {
-        const token = localStorage.getItem('token');
-        if(!token){
-            dispatch(logout());
-        }else{
-            const expirationDate =  new Date(localStorage.getItem('expirationDate'));
-            if(expirationDate > new Date()){
-                dispatch(logout())
-            }else {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
-            }
-        }
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
     }
-}
+  };
+};
